64d5c55498f805a929ab15170c7b070e1d640b43
Hanna Jarlaczyńska <hanna.jarlaczynska@gmail.com>
2020-10-26 21:38:27 +0100

    Do some refactoring

--- create_labirynth.py
+++ create_labirynth.py
@@ -5,19 +5,24 @@ import random
 # 4 x 4 for getting a grasp of what happens
 DIM = 4
 
-cells = [i for i in range(1, DIM*DIM+1)]
+cells = [i for i in range(1, DIM * DIM + 1)]
+
 
 def left(cell):
-    return cell-1
+    return cell - 1
+
 
 def right(cell):
-    return cell+1
+    return cell + 1
+
 
 def up(cell):
-    return cell-DIM
+    return cell - DIM
+
 
 def down(cell):
-    return cell+DIM
+    return cell + DIM
+
 
 def neighbours(cell):
     neighbours = set()
@@ -26,26 +31,26 @@ def neighbours(cell):
 
     if cell % DIM != 0:
         neighbours.add(right(cell))
-    
+
     if cell - DIM > 0:
         neighbours.add(up(cell))
 
-    if cell - DIM*(DIM-1) <= 0:
+    if cell - DIM * (DIM - 1) <= 0:
         neighbours.add(down(cell))
 
     return neighbours
 
 
 def create_edge(cell1, cell2, graph):
+    graph[cell1].add(cell2)
+    graph[cell2].add(cell1)
 
-    pass
 
-
-def create(cells, start):
-    #neighbours_of_cells = {cell : neighbours(cell) for cell in cells}   # probably not useful
+def generate_maze(cells, start, add_timestamps=False):
+    # neighbours_of_cells = {cell : neighbours(cell) for cell in cells}   # probably not useful
     visited = {start}
     stack = [start]
-    graph = {cell : set() for cell in cells}
+    graph = {cell: set() for cell in cells}
 
     while stack:
         current_cell = stack.pop()
@@ -53,27 +58,14 @@ def create(cells, start):
 
         if unvisited_neighbours:
             stack.append(current_cell)
-            [chosen_cell] = random.sample(unvisited_neighbours, 1) #### tutaj zrobić wybieranie losowo
-            
-            #print(graph[current_cell])
-            #print(chosen_cell)
-            #print(graph[chosen_cell])
-            #print(current_cell)
-            graph[current_cell].add(chosen_cell)
-            graph[chosen_cell].add(current_cell)
-            
+            chosen_cell = random.sample(unvisited_neighbours, 1)[0]
+
+            create_edge(chosen_cell, current_cell, graph)
+
             visited.add(chosen_cell)
             stack.append(chosen_cell)
 
     print(graph)
 
-create(cells, 1)
 
-
-
-
-
-
-
-
-    
+generate_maze(cells, 1)
