ec03bd9a2fd785e399b1ca9c5489cac4f9a30aea
Hania <hanna.jarlaczynska@gmail.com>
2020-10-28 17:50:25 +0100

    Display maze

--- create_labirynth.py
+++ create_labirynth.py
@@ -1,9 +1,12 @@
 """First attempt to create dfs algorithm which creates labirynth."""
 
 import random
+import pygame
+#import time
 
 # 4 x 4 for getting a grasp of what happens
-DIM = 4
+DIM = 15
+GRID_SIZE = 20
 
 cells = [i for i in range(1, DIM * DIM + 1)]
 
@@ -46,8 +49,8 @@ def create_edge(cell1, cell2, graph):
     graph[cell2].add(cell1)
 
 
-def generate_maze(cells, start, add_timestamps=False):
-    # neighbours_of_cells = {cell : neighbours(cell) for cell in cells}   # probably not useful
+def generate_maze(cells, start):
+    list_of_edges = []
     visited = {start}
     stack = [start]
     graph = {cell: set() for cell in cells}
@@ -61,11 +64,90 @@ def generate_maze(cells, start, add_timestamps=False):
             chosen_cell = random.sample(unvisited_neighbours, 1)[0]
 
             create_edge(chosen_cell, current_cell, graph)
+            list_of_edges.append((current_cell, chosen_cell))
 
             visited.add(chosen_cell)
             stack.append(chosen_cell)
 
-    print(graph)
+    return list_of_edges, graph
 
 
-generate_maze(cells, 1)
+def which_row(number):
+    for i in range(1, DIM+1):
+        number -= DIM
+        if number <= 0:
+            break
+    row = 2*i - 1
+    return row
+
+
+def which_col(number):
+    if number % DIM == 0:
+        mod = DIM
+    else:
+        mod = number % DIM
+    col = 2*mod - 1
+    return col
+
+
+def rows_to_draw(edge):
+    return [which_row(edge[0]), which_row(edge[1]), int((which_row(edge[0])+which_row(edge[1]))/2)]
+
+
+def cols_to_draw(edge):
+    return [which_col(edge[0]), which_col(edge[1]), int((which_col(edge[0])+which_col(edge[1]))/2)]
+
+
+
+maze_dim = 2*DIM-1
+screen = pygame.display.set_mode((maze_dim*GRID_SIZE,maze_dim*GRID_SIZE))
+
+
+edges_to_draw, graph = generate_maze(cells, 1)
+
+color = (255,255,255)
+
+# edge = (10,11)
+# row1 = which_row(edge[0])
+# col1 = which_col(edge[0])
+# row2 = which_row(edge[1])
+# col2 = which_col(edge[1])
+# row3 = int((row1+row2)/2)
+# col3 = int((col1+col2)/2)
+
+
+# pygame.draw.rect(screen, color, pygame.Rect((col1-1)*GRID_SIZE, (row1-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
+# pygame.display.update()
+
+# pygame.draw.rect(screen, color, pygame.Rect((col2-1)*GRID_SIZE, (row2-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
+# pygame.display.update()
+
+# pygame.draw.rect(screen, color, pygame.Rect((col3-1)*GRID_SIZE, (row3-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
+# pygame.display.update()
+# print(edges_to_draw)
+
+
+#clock = pygame.time.Clock()
+
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    for edge in edges_to_draw:
+        rows = rows_to_draw(edge)
+        cols = cols_to_draw(edge)
+        for i in range(3):
+            pygame.draw.rect(screen, color, pygame.Rect((cols[i]-1)*GRID_SIZE, (rows[i]-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        pygame.display.update()
+        # pygame.time.delay(500)
+        # pygame.display.flip()
+
+    #pygame.display.flip()
+    #clock.tick(30)
+
+
+pygame.quit()
+
