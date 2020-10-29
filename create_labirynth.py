a700bca52a82111b833f3b9b089b5838764819c2
Hania <hanna.jarlaczynska@gmail.com>
2020-10-29 08:12:04 +0100

    Add drawing animation

--- create_labirynth.py
+++ create_labirynth.py
@@ -2,9 +2,8 @@
 
 import random
 import pygame
-#import time
 
-# 4 x 4 for getting a grasp of what happens
+
 DIM = 15
 GRID_SIZE = 20
 
@@ -73,11 +72,11 @@ def generate_maze(cells, start):
 
 
 def which_row(number):
-    for i in range(1, DIM+1):
+    for i in range(1, DIM + 1):
         number -= DIM
         if number <= 0:
             break
-    row = 2*i - 1
+    row = 2 * i - 1
     return row
 
 
@@ -86,68 +85,72 @@ def which_col(number):
         mod = DIM
     else:
         mod = number % DIM
-    col = 2*mod - 1
+    col = 2 * mod - 1
     return col
 
 
 def rows_to_draw(edge):
-    return [which_row(edge[0]), which_row(edge[1]), int((which_row(edge[0])+which_row(edge[1]))/2)]
+    return [
+        which_row(edge[0]),
+        which_row(edge[1]),
+        int((which_row(edge[0]) + which_row(edge[1])) / 2),
+    ]
 
 
 def cols_to_draw(edge):
-    return [which_col(edge[0]), which_col(edge[1]), int((which_col(edge[0])+which_col(edge[1]))/2)]
+    return [
+        which_col(edge[0]),
+        which_col(edge[1]),
+        int((which_col(edge[0]) + which_col(edge[1])) / 2),
+    ]
 
 
+def get_x_coordinates(cols):
+    x_cords = []
+    for i in range(3):
+        x_cords.append((cols[i] - 1) * GRID_SIZE)
+    return x_cords
 
-maze_dim = 2*DIM-1
-screen = pygame.display.set_mode((maze_dim*GRID_SIZE,maze_dim*GRID_SIZE))
+
+def get_y_coordinates(rows):
+    y_cords = []
+    for i in range(3):
+        y_cords.append((rows[i] - 1) * GRID_SIZE)
+    return y_cords
+
+
+maze_dim = 2 * DIM - 1
+screen = pygame.display.set_mode((maze_dim * GRID_SIZE, maze_dim * GRID_SIZE))
 
 
 edges_to_draw, graph = generate_maze(cells, 1)
+counter = 0
 
-color = (255,255,255)
-
-# edge = (10,11)
-# row1 = which_row(edge[0])
-# col1 = which_col(edge[0])
-# row2 = which_row(edge[1])
-# col2 = which_col(edge[1])
-# row3 = int((row1+row2)/2)
-# col3 = int((col1+col2)/2)
-
-
-# pygame.draw.rect(screen, color, pygame.Rect((col1-1)*GRID_SIZE, (row1-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
-# pygame.display.update()
-
-# pygame.draw.rect(screen, color, pygame.Rect((col2-1)*GRID_SIZE, (row2-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
-# pygame.display.update()
-
-# pygame.draw.rect(screen, color, pygame.Rect((col3-1)*GRID_SIZE, (row3-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE)) # arguments in Rect : x,y, dim, dim
-# pygame.display.update()
-# print(edges_to_draw)
-
-
-#clock = pygame.time.Clock()
+color = (255, 255, 255)
 
 
 done = False
 while not done:
+    pygame.time.delay(50)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    for edge in edges_to_draw:
+    if counter < len(edges_to_draw):
+        edge = edges_to_draw[counter]
         rows = rows_to_draw(edge)
         cols = cols_to_draw(edge)
+        x_cords = get_x_coordinates(cols)
+        y_cords = get_y_coordinates(rows)
         for i in range(3):
-            pygame.draw.rect(screen, color, pygame.Rect((cols[i]-1)*GRID_SIZE, (rows[i]-1)*GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(
+                screen,
+                color,
+                pygame.Rect((x_cords[i], y_cords[i], GRID_SIZE, GRID_SIZE)),
+            )
+        counter += 1
         pygame.display.update()
-        # pygame.time.delay(500)
-        # pygame.display.flip()
-
-    #pygame.display.flip()
-    #clock.tick(30)
 
 
 pygame.quit()
-
