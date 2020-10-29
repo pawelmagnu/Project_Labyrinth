8a9efa346898ddf2ced8a744fd244d530f839f55
Kojec1 <konrad.pawlik333@gmail.com>
2020-10-29 10:29:16 +0100

    Improved display

--- PyMazeGenerator.py
+++ PyMazeGenerator.py
@@ -9,14 +9,19 @@ y = 1
 
 print("Podaj rozmiar labiryntu")
 s = input()
+
 screen = pygame.display.set_mode((int(s)*int('20'),int(s)*int('20')))
+pygame.display.set_caption("Project Labyrinth")
 
 grid = {(x,y):1 for x in range(1, int(s)+int('1')) for y in range(1, int(s)+int('1'))}
 
 def MazeLoop(x, y):
     queue.append((x,y))
     visited.append((x,y))
-    grid[(1,1)] = 0
+    
+    pygame.draw.rect(screen, (255,250,250), (0,0,20,20))
+    pygame.display.update()
+
     while len(queue) > 0:
         neighb = []
 
@@ -36,29 +41,57 @@ def MazeLoop(x, y):
 
             if random_neighb == "E":
                 x = x + 2
-                grid[(x,y)] = 0
-                grid[(x-1,y)] = 0
+
+                pygame.draw.rect(screen, (255,250,250), ((x-1)*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
                 queue.append((x,y))
                 visited.append((x,y))
 
             elif random_neighb == "W":
                 x = x - 2
-                grid[(x,y)] = 0
-                grid[(x+1,y)] = 0
+
+                pygame.draw.rect(screen, (255,250,250), ((x+1)*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+                
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
                 queue.append((x,y))
                 visited.append((x,y))
 
             elif random_neighb == "N":
                 y = y + 2
-                grid[(x,y)] = 0
-                grid[(x,y-1)] = 0
+                
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,(y-1)*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
                 queue.append((x,y))
                 visited.append((x,y))
 
             elif random_neighb == "S":
                 y = y - 2
-                grid[(x,y)] = 0
-                grid[(x,y+1)] = 0
+                
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,(y+1)*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
+                pygame.draw.rect(screen, (255,250,250), (x*20-20,y*20-20,20,20))
+                pygame.display.update()
+                time.sleep(.007)
+
                 queue.append((x,y))
                 visited.append((x,y))
 
@@ -66,17 +99,6 @@ def MazeLoop(x, y):
             x, y = queue.pop()
 
 MazeLoop(x,y)
-
-for x in range(1, int(s)+int('1')):
-    for y in range(1, int(s)+int('1')):
-        if grid[(x,y)] == 0:
-            pygame.draw.rect(screen, (255,250,250), (x*20-20,y*20-20,20,20))
-            pygame.display.update()
-            time.sleep(.001)
-        else:
-            pygame.draw.rect(screen, (20,20,20), (x*20-20,y*20-20,20,20))
-            pygame.display.update()
-            time.sleep(.001)
 
 exit = False
 while not exit:
